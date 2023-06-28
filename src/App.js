4e7f013cd00725876e3cb717edd3e768cd012383
Randolph <dhwngjs01@naver.com>
2023-06-28 12:52:40 +0900

    UPDATE - Archiving

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import "swiper/swiper.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/Globals.css";
 
-import Header from "./components/Header";
-import MainVisual from "./components/MainVisual";
-
+import { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Keyboard, Mousewheel } from "swiper";
+
+import Header from "./components/Header";
+import MainVisual from "./components/MainVisual";
 import AboutMe from "./components/AboutMe";
-import { useEffect, useRef, useState } from "react";
 import Skills from "./components/Skills";
+import Archiving from "./components/Archiving";
 
 export default function App() {
   const swiper = useRef(null);
@@ -35,6 +36,49 @@ export default function App() {
     }
   }, [currentSlide]);
 
+  const handlerSlideChangeTransitionEnd = (swiper) => {
+    var acs = document.querySelectorAll(".swiper-slide-active")[0];
+    var hasVerticalScrollbar = acs.scrollHeight > acs.clientHeight;
+
+    if (hasVerticalScrollbar) {
+      var scrollHeight = acs.scrollHeight;
+      var slideSize = acs.swiperSlideSize;
+      var scrollDifferenceTop = scrollHeight - slideSize;
+
+      acs.addEventListener("wheel", findScrollDirectionOtherBrowsers);
+
+      function findScrollDirectionOtherBrowsers(event) {
+        var scrollTop = acs.scrollTop;
+        scrollHeight = acs.scrollHeight;
+        slideSize = acs.swiperSlideSize;
+        var scrollDifference = scrollHeight - (scrollTop + slideSize);
+        var delta;
+
+        if (event.wheelDelta) {
+          delta = event.wheelDelta;
+        } else {
+          delta = -1 * event.deltaY;
+        }
+
+        if (delta < 0) {
+          if (scrollDifference < 1) {
+            swiper.mousewheel.enable();
+          } else {
+            swiper.mousewheel.disable();
+          }
+        } else if (delta > 0) {
+          if (scrollDifference === scrollDifferenceTop) {
+            swiper.mousewheel.enable();
+          } else {
+            swiper.mousewheel.disable();
+          }
+        }
+      }
+    } else {
+      swiper.mousewheel.enable();
+    }
+  };
+
   return (
     <>
       <Header setCurrentSlide={setCurrentSlide} />
@@ -51,48 +95,7 @@ export default function App() {
         onSlideChange={(swiper) => {
           setCurrentSlide(swiper.activeIndex);
         }}
-        onSlideChangeTransitionEnd={(swiper) => {
-          var acs = document.querySelectorAll(".swiper-slide-active")[0];
-          var hasVerticalScrollbar = acs.scrollHeight > acs.clientHeight;
-
-          if (hasVerticalScrollbar) {
-            var scrollHeight = acs.scrollHeight;
-            var slideSize = acs.swiperSlideSize;
-            var scrollDifferenceTop = scrollHeight - slideSize;
-
-            acs.addEventListener("wheel", findScrollDirectionOtherBrowsers);
-
-            function findScrollDirectionOtherBrowsers(event) {
-              var scrollTop = acs.scrollTop;
-              scrollHeight = acs.scrollHeight;
-              slideSize = acs.swiperSlideSize;
-              var scrollDifference = scrollHeight - (scrollTop + slideSize);
-              var delta;
-
-              if (event.wheelDelta) {
-                delta = event.wheelDelta;
-              } else {
-                delta = -1 * event.deltaY;
-              }
-
-              if (delta < 0) {
-                if (scrollDifference < 1) {
-                  swiper.mousewheel.enable();
-                } else {
-                  swiper.mousewheel.disable();
-                }
-              } else if (delta > 0) {
-                if (scrollDifference === scrollDifferenceTop) {
-                  swiper.mousewheel.enable();
-                } else {
-                  swiper.mousewheel.disable();
-                }
-              }
-            }
-          } else {
-            swiper.mousewheel.enable();
-          }
-        }}
+        onSlideChangeTransitionEnd={handlerSlideChangeTransitionEnd}
       >
         <SwiperSlide
           className="main-visual-slide"
@@ -105,6 +108,9 @@ export default function App() {
         </SwiperSlide>
         <SwiperSlide className="skills-slide">
           <Skills />
+        </SwiperSlide>
+        <SwiperSlide className="archiving-slide">
+          <Archiving />
         </SwiperSlide>
       </Swiper>
     </>
