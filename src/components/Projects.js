8484454ee3c662103bcf1a24c7c3642186c8942e
= <dhwngjs01@naver.com>
2023-12-14 03:11:24 +0900

    refactor: Proejct 섹션 컴포넌트화, 섹션 제목 코드 단축

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,45 +6,51 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 export default function Projects() {
   return (
     <section id="projects">
-      <div className="projects py-5">
-        <Container>
-          <h2 className="projects-title section-title text-center text-white">
-            <FontAwesomeIcon icon={faLink} className="icon" />
-            <span>PROJECTS</span>
-          </h2>
-          <div className="projects-content">
-            <Row className="gy-4">
-              <Col lg={12}>
-                <Card className="p-4 rounded-4 shadow">
-                  <Card.Title className="text-center">
-                    <h2 className="fs-2">Electrip</h2>
-                    <p className="fs-6">2023. 03 (개인 프로젝트)</p>
-                  </Card.Title>
-                  <Card.Body></Card.Body>
-                </Card>
-              </Col>
-              <Col lg={12}>
-                <Card className="p-4 rounded-4 shadow">
-                  <Card.Title className="text-center">
-                    <h2 className="fs-2">Oh! Bike 쇼핑몰</h2>
-                    <p className="fs-6">2019. 11 (개인 프로젝트)</p>
-                  </Card.Title>
-                  <Card.Body></Card.Body>
-                </Card>
-              </Col>
-              <Col lg={12}>
-                <Card className="p-4 rounded-4 shadow">
-                  <Card.Title className="text-center">
-                    <h2 className="fs-2">바이크 중고거래 게시판</h2>
-                    <p className="fs-6">2019.09 (개인 프로젝트)</p>
-                  </Card.Title>
-                  <Card.Body></Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </div>
-        </Container>
-      </div>
+      <Container>
+        <h2 className="projects-title section-title text-white">
+          <FontAwesomeIcon icon={faLink} className="icon" />
+          <span>PROJECTS</span>
+        </h2>
+        <div className="projects-content">
+          <Row className="gy-4">
+            <Col lg={12}>
+              {ProjectCard({
+                title: "Electrip",
+                date: "2023. 03",
+                collaboration: "개인 프로젝트",
+              })}
+            </Col>
+            <Col lg={12}>
+              {ProjectCard({
+                title: "Oh! Bike 쇼핑몰",
+                date: "2019. 11",
+                collaboration: "개인 프로젝트",
+              })}
+            </Col>
+            <Col lg={12}>
+              {ProjectCard({
+                title: "바이크 중고거래 게시판",
+                date: "2019. 09",
+                collaboration: "개인 프로젝트",
+              })}
+            </Col>
+          </Row>
+        </div>
+      </Container>
     </section>
+  );
+}
+
+export function ProjectCard({ title, date, collaboration, image }) {
+  return (
+    <Card className="p-4 rounded-4 shadow">
+      <Card.Title className="text-center">
+        <h2 className="fs-2">{title}</h2>
+        <p className="fs-6">
+          {date} ({collaboration})
+        </p>
+      </Card.Title>
+      <Card.Body></Card.Body>
+    </Card>
   );
 }
