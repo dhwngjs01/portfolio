7bdd96a9b5cc885c03b6217737f8083adf32faf2
= <dhwngjs01@naver.com>
2023-12-15 23:21:46 +0900

    feat: 스크롤 메뉴 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "swiper/swiper.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/Globals.css";
 
@@ -8,42 +7,24 @@ import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
 import Archiving from "./components/Archiving";
 import Projects from "./components/Projects";
-import { useEffect } from "react";
+import Career from "./components/Career";
 
 export default function App() {
-  useEffect(() => {
-    const nav = document.querySelector("#nav");
-    const navLink = document.querySelectorAll(".nav-link");
-
-    const aboutMe = document.querySelector("#about-me");
-    const aboutmeTop = aboutMe.offsetTop;
-    const aboutmeHeight = aboutMe.offsetHeight;
-    const aboutmeBottom = aboutmeTop + aboutmeHeight;
-
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 0) {
-        nav.classList.add("navbar-scroll");
-      } else {
-        nav.classList.remove("navbar-scroll");
-      }
-
-      // about me active class toggle
-      if (window.scrollY >= aboutmeTop && window.scrollY < aboutmeBottom) {
-        navLink[0].classList.add("active");
-      } else {
-        navLink[0].classList.remove("active");
-      }
-    });
-  }, []);
+  // 스크롤 이동 함수
+  const handleClickLinkIcon = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <>
-      <Header />
-      <MainVisual />
-      <AboutMe />
-      <Skills />
-      <Archiving />
-      <Projects />
+      <Header handleClickLinkIcon={handleClickLinkIcon} />
+      <MainVisual handleClickLinkIcon={handleClickLinkIcon} />
+      <AboutMe handleClickLinkIcon={handleClickLinkIcon} />
+      <Skills handleClickLinkIcon={handleClickLinkIcon} />
+      <Archiving handleClickLinkIcon={handleClickLinkIcon} />
+      <Projects handleClickLinkIcon={handleClickLinkIcon} />
+      <Career handleClickLinkIcon={handleClickLinkIcon} />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,60 @@
-import React from "react";
+import { React, useEffect } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 import "../styles/Header.css";
 
-export default function Header({ setCurrentSlide }) {
-  const menu_list = ["About Me", "Skills", "Archiving", "Projects", "Career"];
+export default function Header() {
+  const menu_list = [
+    { id: "about-me", name: "About Me" },
+    { id: "skills", name: "Skills" },
+    { id: "archiving", name: "Archiving" },
+    { id: "projects", name: "Projects" },
+    { id: "career", name: "Career" },
+  ];
+
+  useEffect(() => {
+    const nav = document.querySelector("#nav");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        nav.classList.add("navbar-scroll");
+      } else {
+        nav.classList.remove("navbar-scroll");
+      }
+
+      // 스크롤 위치에 따른 메뉴 활성화
+      const scrollPosition = window.scrollY;
+      const sectionList = document.querySelectorAll("section");
+      sectionList.forEach((section) => {
+        if (scrollPosition >= section.offsetTop - 100 && scrollPosition < section.offsetTop + section.offsetHeight - 100) {
+          const id = section.getAttribute("id");
+
+          document.querySelectorAll(".navbar-nav a").forEach((a) => {
+            a.classList.remove("active");
+          });
+
+          document.querySelector(`a[data-id="${id}"]`).classList.add("active");
+        }
+      });
+    });
+  }, []);
+
+  // 스크롤 이동 함수
+  const handleClick = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
     <header className="header">
       <Navbar expand="lg" fixed="top" id="nav">
         <Container className="justify-content-between">
-          <Nav.Link className="fw-bold fs-3 text-white-70" onClick={() => setCurrentSlide(0)}>
+          <Nav.Link
+            className="fw-bold fs-3 text-white-70"
+            onClick={() => {
+              handleClick("root");
+            }}
+          >
             포트폴리오
           </Nav.Link>
           <Navbar.Toggle aria-controls="navbarScroll" />
@@ -19,8 +63,8 @@ export default function Header({ setCurrentSlide }) {
               {menu_list.map((menu, index) => {
                 return (
                   <Nav.Item key={index} className="px-lg-3">
-                    <Nav.Link className="fw-bold fs-5 text-white-70" onClick={() => setCurrentSlide(index + 1)}>
-                      {menu}
+                    <Nav.Link data-id={menu.id} className="fw-bold fs-5 text-white-70" onClick={() => handleClick(menu.id)}>
+                      {menu.name}
                     </Nav.Link>
                   </Nav.Item>
                 );
